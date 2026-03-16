c23daed70e0698ea98579f2bccfdc9d486945dd8
sans690 <scsmith7255@gmail.com>
2026-03-16 16:36:57 -0400

    Upload

--- EmployeeScheduling.cs
+++ EmployeeScheduling.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Linq;
 
 class EmployeeScheduling
 {
@@ -14,9 +15,9 @@ class EmployeeScheduling
     {
         Console.WriteLine("\n1. Add Employee");
         Console.WriteLine("2. View Employees");
-        Console.WriteLine("3. Assign Shift");
-        Console.WriteLine("4. View Schedule");
-        Console.WriteLine("5. Remove Employee");
+        Console.WriteLine("3. Remove Employee");
+        Console.WriteLine("4. Assign Shift");
+        Console.WriteLine("5. View Schedule");
         Console.WriteLine("6. Exit\n");
 
         string? result = Console.ReadLine();
@@ -24,12 +25,40 @@ class EmployeeScheduling
         switch (result)
         {
             case "1":
-                AddEmployee();
+                try
+                {
+                    AddEmployee();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
                 Menu();
                 break;
 
             case "2":
-                ViewEmployees();
+                try
+                {
+                    ViewEmployees();
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+                Menu();
+                break;
+
+            case "3":
+                try
+                {
+                    RemoveEmployee();
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
                 Menu();
                 break;
 
@@ -46,6 +75,12 @@ class EmployeeScheduling
         Employee employee = new Employee();
         Console.WriteLine("Enter a name");
         employee.Name = Console.ReadLine();
+
+        if (employee.Name.Any(char.IsDigit))
+        {
+            throw new Exception("\nInvalid input entered");
+        }
+
         employee.Id = random.Next(1000, 2000);
         Console.WriteLine($"Name: {employee.Name}, ID: {employee.Id} was entered");
         EmployeeList.Add(employee);
@@ -53,12 +88,38 @@ class EmployeeScheduling
 
     public static void ViewEmployees()
     {
-        foreach (var e in EmployeeList)
+        if (EmployeeList.Count() == 0)
         {
-            Console.WriteLine($"Name: {e.Name}, ID: {e.Id}");
+            throw new Exception("\n No employees for you to view");
+        }
+
+        foreach (var employee in EmployeeList)
+        {
+            Console.WriteLine($"Name: {employee.Name}, ID: {employee.Id}");
         }
     }
 
+    public static void RemoveEmployee()
+    {
+        if (EmployeeList.Count() == 0)
+        {
+            throw new Exception("\nNo employees for you to remove");
+        }
+
+        Console.WriteLine("Enter ID of employee that you want to remove: ");
+        string? result = Console.ReadLine();
+        int.TryParse(result, out int intResult);
+        for (int e = EmployeeList.Count - 1; e >= 0; e--)
+        {
+            {
+                if (EmployeeList[e].Id == intResult)
+                {
+                    Console.WriteLine($"\n{EmployeeList[e].Name} was removed");
+                    EmployeeList.Remove(EmployeeList[e]);
+                }
+            }
+        }
+    }
 
     class Employee
     {
